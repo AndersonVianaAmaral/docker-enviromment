c21d0bd1317dc9a568e8d8653d9f1ef715be90de
anderson.viana.amaral <anderson.viana.amaral@gmail.com>
2020-08-26 10:24:10 -0300

    ports changed

--- nginx/files/nginx.sh
+++ nginx/files/nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-dockerize -template ./fileToChange/nginx.conf:nginx.conf -wait tcp://php-app:8000 -timeout 120s
+dockerize -template ./fileToChange/nginx.conf:nginx.conf -wait tcp://php-app:9000 -timeout 120s
 nginx -g "daemon off;"
--- src/files/entrypoint.sh
+++ src/files/entrypoint.sh
@@ -3,4 +3,4 @@ composer install
 php artisan key:generate 
 dockerize -template ./ENTRYPOINT/.env:.env -wait tcp://database:3306 -timeout 40s
 php artisan migrate
-php artisan serve --host=0.0.0.0 --port=8000
+php artisan serve --host=0.0.0.0 --port=9000
